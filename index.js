f33e3c5f56cf32baf2dfd4ef77a55a615acc66b1
Horakhsh Sharifi <horakhshsharifi1@gmail.com>
2023-07-15 12:45:13 +0500

    second commit

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("numberofplayer")
-console.log(numberofplayer)
-let count = 0
+
+let count = 0;
 function increment() {
     count = count + 1
     countEl.innerText = count
 }
 
+function savePlayers() {
+  let countStr = count + " - "
+  saveEl.textContent += countStr
+}
+
 
 
 const restBtn = document.getElementById('restbtn');
-
 restBtn.addEventListener('click', () => {
   location.reload();
 });
-
-
-const increaseBtn = document.getElementById('increaseBtn');
-        const imageContainer = document.getElementById('imageContainer');
-        let imageCount = 0;
-        function increaseImageCount() {
-          imageCount++;
-          const imageElement = document.createElement('img');
-          imageElement.src = 'images.jpeg';
-          imageElement.classList.add('thumbnail');
-          imageContainer.appendChild(imageElement);
-        }
-        increaseBtn.addEventListener('click', increaseImageCount);
 
 
 
